771618016ac2419e2dc7417234ea0f68c83da0c4
Yanling Zhang <yanling.zhang@ibm.com>
2020-01-15 11:39:12 -0600

    Added tests to verify state and adding items to catalog
    Co-authored-by: Shaun Colley <shaun.colley@ibm.com>

--- client/src/pattern-components/add-to-catalog/AddCatalog.spec.js
+++ client/src/pattern-components/add-to-catalog/AddCatalog.spec.js
@@ -2,29 +2,37 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import AddToCatalog from './AddCatalog';
 import "../../setupTests"
-
-let catalog = [];
 let item = {
   name: 'bacon',
   size: 20,
   comment: 'more! 20 pounds Janice!',
 }
 let addToCatalog = () => {
-  catalog.push(item)
-  return catalog
+  state.catalog.push(item);
+  return state;
 }
-
+let state = {
+  catalog: [],
+};
 
 describe('AddToCatalog', () => {
   it('canary test shows the infrastructure works', () => {
     expect(true).toEqual(true);
   });
 
+  it('adding item should be added into state', () => {
+    expect((state.catalog.length)).toEqual(0);
+  });
+
   it('catalog length should be greater than 0', () => {
-    expect(addToCatalog().length).toBeGreaterThan(0);
+    expect(addToCatalog().catalog.length).toBeGreaterThan(0);
   })
 
-
+  it('adding an item into state should make the catalog length grow', () => {
+    let a = state.catalog.length;
+    addToCatalog();
+    expect((state.catalog.length)).toBeGreaterThan(a);
+  });
 
   let wrapper;
   beforeEach(() => wrapper = shallow(<AddToCatalog />));
