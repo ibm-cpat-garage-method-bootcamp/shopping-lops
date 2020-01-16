40f7a8c2c5b0a9d9c73e64dc0e68b803a52c2a37
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-01-16 14:36:36 -0600

    Worked on UI for adding stores to item being added to catalog.

--- client/src/pattern-components/add-to-catalog/AddCatalog.jsx
+++ client/src/pattern-components/add-to-catalog/AddCatalog.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { GroceryStore } from "../../models/GroceryStore.ts";
 import { Item } from "../../models/Item.ts";
 import { Catalog } from "../../models/Catalog.ts";
-
+import { GroceryStore} from "../../models/GroceryStore.ts";
+import  "./AddToCatalog.scss";
 class AddToCatalog extends Component {
   constructor() {
     super()
@@ -36,50 +36,72 @@ class AddToCatalog extends Component {
       })
     } else {
       let updatedCatalog = this.state.catalog;
-      const item = new Item (this.state.itemName, this.state.itemQuantity, this.state.itemComment)
+      let stores = [];
+
+      for(let i = 0; i < this.state.storeCount; i++) {
+
+        // let store = new GroceryStore(this.state.store, this.state.aisle);
+        // stores.push()
+      }
+
+      const item = new Item (this.state.itemName, this.state.itemQuantity, this.state.itemComment, stores)
       updatedCatalog.push(item);
       return this.setState({
         updatedCatalog,
         message: 'Thank you for submitting an item',
       })
     }
-  }
+  };
 
   handleAddStoreClick = async (e) => {
     e.preventDefault();
     await this.setState({
       storeCount: this.state.storeCount + 1,
-    })
-    console.log(this.state.storeCount)
-  }
+    });
+  };
 
 
   render() {
     let groceryStoreInputs = [];
     for (let i = 0; i < this.state.storeCount; i++) {
       groceryStoreInputs.push(
-        <div>
-          <label>Grocery Store Name: </label>
-          <input name="itemQuantity" type="text" onChange={(e) => this.handleInputChange(e)} />
-          <label>Aisle Number: </label>
-          <input name="itemComment" type="text" onChange={(e) => this.handleInputChange(e)} />
+        <div className="grocery-form">
+          <label>Store Name: </label>
+          <input name={`store${i}`} type="text" onChange={(e) => this.handleInputChange(e)} />
+          <label className="aisle-label">Aisle #: </label>
+          <input className="aisle-input" name={`aisle${i}`} type="text" onChange={(e) => this.handleInputChange(e)} />
         </div>
       )
     }
 
-
     return (
-      <div className="awesomeDiv">
-        <form >
-          <label>Item Name: </label>
-          <input name="itemName" type="text" onChange={(e) => this.handleInputChange(e)} />
-          <label>Item Quantity: </label>
-          <input name="itemQuantity" type="text" onChange={(e) => this.handleInputChange(e)} />
-          <label>Comment: </label>
-          <input name="itemComment" type="text" onChange={(e) => this.handleInputChange(e)} />
-          <button onClick={(e) => this.handleAddStoreClick(e)}>+</button>
+      <div className="catalog-form">
+        <form>
+          <div>
+            <label>Item Name: </label>
+            <input name="itemName" type="text" onChange={(e) => this.handleInputChange(e)} />
+          </div>
+          <div>
+            <label>Item Quantity: </label>
+            <input name="itemQuantity" type="text" onChange={(e) => this.handleInputChange(e)} />
+          </div>
+          <div>
+            <label>Comment: </label>
+            <input name="itemComment" type="text" onChange={(e) => this.handleInputChange(e)} />
+          </div>
+
+
           {groceryStoreInputs}
-          <button className="submitBtn" onClick={(e)=> this.handleSubmitClick(e)}>Submit</button>
+
+
+          <div>
+            <button onClick={(e) => this.handleAddStoreClick(e)}>Add Store</button>
+            <button className="submitBtn" onClick={(e)=> this.handleSubmitClick(e)}>Submit</button>
+          </div>
+
+
+
+
         </form>
         <p>
           {this.state.message}
